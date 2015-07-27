9092669d5f2ca5c8e4d9436ade84f2449ad933de
dungtq <dungtq@rikkeisoft.com>
2015-07-27 09:37:34 +0700

    Add model Unit process data in table unit

---
+++ app/Model/Unit.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Unit
+ *
+ * @author dungtq
+ */
+class Unit extends AppModel
+{
+
+    /**
+     * get Unit by id
+     * 
+     * @param int $id Unit id
+     * @return array|null
+     */
+    public function getUnitById($id)
+    {
+        return $this->findById($id);
+    }
+
+    /**
+     * get all unit records
+     * 
+     * @return array | null
+     */
+    public function getListUnit()
+    {
+        return $this->find('all');
+    }
+
+}
